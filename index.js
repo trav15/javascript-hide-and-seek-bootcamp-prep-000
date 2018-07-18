d0e7be1e7700e2fd520f996314c1610aff7dba69
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 15:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
 
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
+  for (let i = 0, i < rankedLists.length; i++) {
     let children = rankedLists[i].children
 
     for (let j = 0, k = children.length; j < k; j++) {

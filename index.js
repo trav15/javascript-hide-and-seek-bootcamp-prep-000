f9532bcf65373d1a777e587fffe5804948824bf4
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 14:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ function increaseRankBy(n) {
     }
   }
 }
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
